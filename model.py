3f430d21dbd7379b8a815f80d29f7dac166de7e0
ASK <amey1695@gmail.com>
2024-10-10 17:42:59 -0600

    Added proj layer

--- model.py
+++ model.py
@@ -202,3 +202,14 @@ class Decoder(nn.Module):
         
         return self.norm(x)
         
+
+
+ class ProjectionLayer(nn.Module):
+    def __init__(self, d_model:int, vocab_size:int) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, vocab_size)
+        return torch.log_softmax(self.proj(x), dim = -1)
+
