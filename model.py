f3b0ebcc7f8cc8fe6801b9b080325dfb14f85286
ASK <amey1695@gmail.com>
2024-10-10 15:50:03 -0600

    Added feedforward

--- model.py
+++ model.py
@@ -60,5 +60,20 @@ class LayerNormalization(nn.Module):
         return self.alpha * ((x-mean)/(std+self.eps)) + self.bias
 
 
+class FeedForwardBlock(nn.Module):
+    
+    def __init__(self, d_model:int, d_ff:int, dropout:float):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # W2 and B2
+
+    
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_ff) --> (Batch, seq_len, d_model)
+        # The above happens first through transformation with (W1, B1) and then with (W2, B2)
+
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
 
 
